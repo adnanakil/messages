cc3d35c987b331b099048b9f496ac8a46c879090
Adnan Akil <adnanakil@gmail.com>
2014-11-16 14:47:51 -0800

    second commit

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
  
@@ -12,6 +13,23 @@ def hello_monkey():
     return str(resp)
 
 
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "AC290582373b22d8217957a62974d75ce8"
+auth_token  = "1037caa6e137bc8d996a98c0acb5c025"
+client = TwilioRestClient(account_sid, auth_token)
+ 
+
+@app.route("/send")
+def send_message():
+	message = client.messages.create(body="HELLO?! IS ANYBODY THERE?!",
+    to="+12017570419",
+    from_="+14158814524")
+	print message.sid
+	return message.sid
+
+
  
 if __name__ == "__main__":
     app.run(debug=True)
+
+
