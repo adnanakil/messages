a459cede73671acff41a933a836d458902085889
Adnan Akil <adnanakil@gmail.com>
2014-11-16 21:18:46 -0800

    added caller name support

--- run.py
+++ run.py
@@ -5,12 +5,34 @@ from twilio.rest import TwilioRestClient
 app = Flask(__name__)
  
 
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+    """Respond and greet the caller by name."""
+ 
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
+
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message(message)
+ 
     return str(resp)
+
+
+# @app.route("/", methods=['GET', 'POST'])
+# def hello_monkey():
+#     """Respond to incoming calls with a simple text message."""
+#     resp = twilio.twiml.Response()
+#     resp.message("Hello, Mobile Monkey")
+#     return str(resp)
+
+
+
+
+
 
 
 # Your Account Sid and Auth Token from twilio.com/user/account
@@ -18,18 +40,20 @@ account_sid = "AC290582373b22d8217957a62974d75ce8"
 auth_token  = "1037caa6e137bc8d996a98c0acb5c025"
 client = TwilioRestClient(account_sid, auth_token)
  
-
-@app.route("/send")
-def send_message():
-	message = client.messages.create(body="HELLO?! IS ANYBODY THERE?!",
-    to="+12017570419",
-    from_="+14158814524")
-	print message.sid
-	return message.sid
+# Try adding your own number to this list!
+callers = {
+    "+12017570419": "Curious George"
+}
 
 
- 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
+# @app.route("/send")
+# def send_message():
+#   message = client.messages.create(body="HELLO?! IS ANYBODY THERE?!",
+#     to="+12017570419",
+#     from_="+14158814524")
+#   print message.sid
+#   return message.sid
